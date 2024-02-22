28efaa0b4b74ed83fe9692b6edf1523f49294bfc
Apurbo Roy <apurboroy7077@gmail.com>
2024-02-22 22:50:50 +0600

    Title Metadata, lesson 18, Date, 22/2/2024, Thursday, 10:50 PM

--- hello_world/src/app/blog/second/page.tsx
+++ hello_world/src/app/blog/second/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Metadata } from "next";
 
+const metadata: Metadata = {
+  title: {
+    absolute: "This is Blog",
+  },
+  description: "hello",
+};
 const Second = () => {
   return <div>Second</div>;
 };
-
+export { metadata };
 export default Second;
