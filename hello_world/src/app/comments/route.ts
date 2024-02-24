4b2625d443e0b0839d8c839f5ae4152c0273dc80
Apurbo Roy <apurboroy7077@gmail.com>
2024-02-24 23:16:52 +0600

    Query Parameters, Lesson 39, Date 24/02/2024, Saturday, 11:16 PM, Night.

--- hello_world/src/app/comments/route.ts
+++ hello_world/src/app/comments/route.ts
@@ -1,20 +1,21 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-let GET = () => {
-  return Response.json(comments);
+type propsType = {
+  params: { id: string };
 };
-let POST = async (request: Request) => {
-  let comment = await request.json();
-  let newComment = {
-    id: comments.length + 1,
-    text: comment.text,
-  };
-  comments.push(newComment);
-  return new Response(JSON.stringify(newComment), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    status: 201,
-  });
+let GET = (request: NextRequest) => {
+  let searchParams = request.nextUrl.searchParams;
+  let query = searchParams.get("query") as string;
+  console.log(query);
+  if (!query) {
+    return Response.json(comments);
+  } else {
+    let filteredComments = comments.filter((comment) =>
+      comment.text.includes(query)
+    );
+    return Response.json(filteredComments);
+  }
 };
-export { GET, POST };
+
+export { GET };
