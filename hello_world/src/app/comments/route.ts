ffd52446275ca1c878fb134e74f5eea6c75198a8
Apurbo Roy <apurboroy7077@gmail.com>
2024-02-24 19:49:47 +0600

    Handling POST Request, Lesson 35, Date: 24/2/2024, Saturday, 7:49 PM, Night.

--- hello_world/src/app/comments/route.ts
+++ hello_world/src/app/comments/route.ts
@@ -3,5 +3,18 @@ import { comments } from "./data";
 let GET = () => {
   return Response.json(comments);
 };
-
-export { GET };
+let POST = async (request: Request) => {
+  let comment = await request.json();
+  let newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+};
+export { GET, POST };
