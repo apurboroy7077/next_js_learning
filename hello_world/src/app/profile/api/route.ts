bd50cf8d5c7a2b70bb5921bd3a5da0fa187ea265
Apurbo Roy <apurboroy7077@gmail.com>
2024-02-25 09:45:51 +0600

    Cookies in Route Handlers, Lesson 42, Date: 25/02/2024, Sunday, 9:45 AM, Day.

--- hello_world/src/app/profile/api/route.ts
+++ hello_world/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 let GET = async (request: NextRequest) => {
@@ -6,9 +6,15 @@ let GET = async (request: NextRequest) => {
   console.log(requestHeaders.get("Authorization"));
   let headerList = headers();
   console.log(headerList.get("Authorization"));
+
+  cookies().set("resultsPerPage", "20"); // You can also set cookies like this
+  console.log(cookies().get("resultsPerPage")); //this will console log the cookie named "resultsPerPage"
+  let theme = request.cookies.get("theme"); // TO load any cookie if it is already there
+  console.log(theme);
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark", // This Will Set the Cookie
     },
   });
 };
